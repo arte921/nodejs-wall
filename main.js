3bc9dcd083947b95758050ad74374bae99c0e675
Ard-Jan van Etten <arte@linux.fritz.box>
2020-02-04 20:16:37 +0100

    f

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 var http = require('http');
+var fs = require('fs');
 
-function onres(req,res){
-  res.write('helo');
-  res.end();
-  console.log('h');
-}
+var page;
 
 http.createServer(function (req,res){
-  onres(req,res);
+
+  try{
+    page = fs.readFileSync('.' + req.url).toString();
+  }catch{
+    page = '404';
+  }
+
+
+  console.log(req.url);
+
+
+  res.write(page);
+  res.end();
 }).listen(2109);
 
 console.log('started');
